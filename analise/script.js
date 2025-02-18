ed3022a8aaa21c7988586e5be452648e4423d82d
comininick <160253618+comininick@users.noreply.github.com>
2025-02-18 08:00:39 -0300

    java

---
+++ analise/script.js
@@ -0,0 +1,20 @@
+var nome ;
+var endereco ;
+var cartao ;
+var senha ;
+
+
+//alert(aluno);
+
+
+//console.log(aluno);
+
+
+nome= prompt("Digite seu nome");
+endereco= prompt("Digite seu endereço");
+cartao= prompt("Digite o número do seu cartão");
+senha= prompt("Digite a senha do se cartão ");
+
+
+console.log(nome,endereco,cartao,senha);
+
